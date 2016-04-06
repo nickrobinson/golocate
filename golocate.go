fd2ef26af059d3d85be34f96591fbf98da899206
Nick Robinson <nickrobinson@gatech.edu>
2016-04-05 20:58:36 -0400

    Adding tests

---
+++ golocate.go
@@ -0,0 +1,71 @@
+package golocate
+
+import (
+	"net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+//https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDu723BTsqPUzpSQHqDHrOZTThMbPe13_Y
+
+type WifiAccessPoints struct {
+	MacAddress string `json:"macAddress"`
+	SignalStrength int `json:"signalStrength"`
+	Age int `json:"age"`
+	Channel int `json:"channel"`
+	SignalToNoiseRatio int `json:"signalToNoiseRatio"`
+}
+
+type CellTowers struct {
+	CellId int `json:"cellId"`
+	LocationAreaCode int `json:"locationAreaCode"`
+	MobileCountryCode int `json:"mobileCountryCode"`
+	MobileNetworkCode int `json:"mobileNetworkCode"`
+}
+
+type Request struct {
+	HomeMobileCountryCode int `json:"homeMobileCountryCode"`
+	HomeMobileNetworkCode int `json:"homeMobileNetworkCode"`
+	RadioType string `json:"radioType"`
+	Carrier string `json:"carrier"`
+	WifiAccessPoints []WifiAccessPoints `json:"wifiAccessPoints"`
+	CellTowers []CellTowers `json:"cellTowers"`
+}
+
+type GeoClient struct {
+	Url string
+	ApiKey string
+}
+
+func NewGeoClient(key string) *GeoClient {
+	gc := GeoClient{
+		Url: "https://www.googleapis.com/geolocation/v1/geolocate?key=",
+		ApiKey: key,
+	}
+	return &gc
+}
+
+func (gc *GeoClient) getRequestUrl() string {
+	url := (gc.Url + gc.ApiKey)
+	return url
+}
+
+func (gc *GeoClient) SendRequest(reqest Request) *http.Response {
+	jsonStr, _ := json.Marshal(reqest)
+	req, err := http.NewRequest("POST", gc.getRequestUrl(), bytes.NewBuffer(jsonStr))
+	fmt.Print(req)
+	if err != nil {
+		fmt.Println(req)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	return resp
+}
---
+++ golocate_test.go
@@ -0,0 +1,56 @@
+package golocate
+
+import (
+	"testing"
+	"fmt"
+	"io/ioutil"
+)
+
+func TestWifiAccessPointsContruct(t *testing.T) {
+	wap := WifiAccessPoints{
+		MacAddress: "00:04:F2:80:81:BC",
+		SignalStrength: -34,
+		Age: 0,
+		Channel: 11,
+		SignalToNoiseRatio: -34,
+	}
+
+	if wap.MacAddress != "00:04:F2:80:81:BC" {
+		t.Error("Expected 00:04:F2:80:81:BC, got ", wap.MacAddress)
+	}
+
+}
+
+func TestGeoClientUrl(t *testing.T) {
+	gc := NewGeoClient("TestApiString")
+	if gc.getRequestUrl() != "https://www.googleapis.com/geolocation/v1/geolocate?key=TestApiString" {
+		t.Error("Expected https://www.googleapis.com/geolocation/v1/geolocate?key=TestApiString, got ", gc.getRequestUrl())
+	}
+}
+
+func TestSimpleRequest(t *testing.T) {
+	gc := NewGeoClient("AIzaSyDu723BTsqPUzpSQHqDHrOZTThMbPe13_Y")
+	wap := []WifiAccessPoints{WifiAccessPoints{
+		MacAddress: "01:23:45:67:89:AB",
+		SignalStrength: 8,
+		Age: 0,
+		Channel: 8,
+		SignalToNoiseRatio: -65,
+	}}
+	req := Request{
+		HomeMobileCountryCode: 310,
+		HomeMobileNetworkCode: 260,
+		RadioType: "gsm",
+		Carrier: "T-Mobile",
+		WifiAccessPoints: wap,
+	}
+
+	resp := gc.SendRequest(req)
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(contents)
+	}
+}
